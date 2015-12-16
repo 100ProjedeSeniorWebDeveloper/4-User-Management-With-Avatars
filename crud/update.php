7b3d0a6a4634c7c5e8d25deda8901ce511e63790
Barış Esen <besen14@posta.pau.edu.tr>
2015-12-16 17:27:35 +0200

    Update update.php

--- crud/update.php
+++ crud/update.php
@@ -9,25 +9,45 @@ $id=$_GET["id"];
 	$musteri->execute(array($id));
 $count=$musteri->rowCount();
 if($count>0){
+			
+			
+
+
+if(isset($_POST["ad"]) && isset($_POST["soyad"]) && isset($_POST["cep"]) && isset($_POST["email"]) && isset($_POST["ek"]))
+{
+	$ad=$_POST["ad"];
+	$soyad=$_POST["soyad"];
+	$cep=$_POST["cep"];
+	$email=$_POST["email"];
+	$ek=$_POST["ek"];
+
+	include ("upload.php");
+
+	$update = $db->prepare("UPDATE kullanici_bilgileri SET ad=?, soyad=?, cep=?, email=?, ek=?, image_200_200=?, image_80_80=?, image_300_300=? WHERE id=?");
+	$update->execute(array($ad, $soyad, $cep, $email, $ek, $name_200_200,$name_80_80,$name_300_300,$id));
+	if($update)
+	{	
+	
+		header("location:index.php");
+		unlink($sil1);
+ 		unlink($sil2);
+ 		unlink($sil3);
+		
+}	
+}
+foreach($musteri as $row)
+			{
+			$sil1=$row["image_80_80"];
+			$sil2=$row["image_200_200"];
+			$sil3=$row["image_300_300"];
+
 ?>
+
 <form action="" method="post" enctype="multipart/form-data">
 <h2>Müsteri Düzenle</h2>
-	<table cellpadding="10">
-		
-		<?php 
-		
-
-		if($musteri)
-			
-			foreach($musteri as $row)
-			{
-						$sil1=$row["image_80_80"];
-						$sil2=$row["image_200_200"];
-						$sil3=$row["image_300_300"];
-		 ?>
-			
+	<table cellpadding="10">		
 			<tr>
-				<td><div><img src="<?=$row['image_200_200']; ?>" alt=""></div></td>
+				<td><img src="<?=$row['image_200_200']; ?>" alt=""></td>
 				<td>Ad:</td>
 				<td><input type="text" name="ad" value="<?=$row['ad'];?>"></td>
 				<td>Soyad:</td>
@@ -44,36 +64,14 @@ if($count>0){
 				<td></td>
 				<td><input type="submit" value="Update"></td>				
 			</tr>
-		
-		<?php 
+	</table>
+</form>
+<?php 
+
 			}
 		}else{
 			echo "Sayfa Bulunamadı. Anasayfaya Yönlendiriliyorsunuz..";
 			header("refresh:2; url=index.php");
 		}
 		
-
-if(isset($_POST["ad"]) && isset($_POST["soyad"]) && isset($_POST["cep"]) && isset($_POST["email"]) && isset($_POST["ek"]))
-{
-	$ad=$_POST["ad"];
-	$soyad=$_POST["soyad"];
-	$cep=$_POST["cep"];
-	$email=$_POST["email"];
-	$ek=$_POST["ek"];
-
-	include ("upload.php");
-
-	$update = $db->prepare("UPDATE kullanici_bilgileri SET ad=?, soyad=?, cep=?, email=?, ek=?, image_200_200=?, image_80_80=?, image_300_300=? WHERE id=?");
-	$update->execute(array($ad, $soyad, $cep, $email, $ek, $name_200_200,$name_80_80,$name_300_300,$id));
-	if($update)
-	{	
-		header("location:index.php");
-		unlink($sil1);
- 		unlink($sil2);
- 		unlink($sil3);
-		
-}
-}
-		 ?>
-	</table>
-</form>
+?>
